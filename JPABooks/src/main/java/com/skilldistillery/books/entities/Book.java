c1fd3eac26bd3006c089ef7c9c27ae82c3908b25
Liz <lizbaze4730@gmail.com>
2023-01-20 16:55:51 -0600

    Build JPA project, stub Book entity, add gradle, config persistencexml

---
+++ JPABooks/src/main/java/com/skilldistillery/books/entities/Book.java
@@ -0,0 +1,92 @@
+package com.skilldistillery.books.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String  title;
+	
+	private String author;
+	
+	
+	
+	public Book() {
+		
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+
+	public void setName(String title) {
+		this.title = title;
+	}
+
+
+
+	public String getAuthor() {
+		return author;
+	}
+
+
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return id == other.id;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", Title=" + title + ", author=" + author + "]";
+	}
+
+}
